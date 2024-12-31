dd01617e5fdecf185a78068f0af8ce9717c46411
aykanavsar <aykan.avsar@gmail.com>
2024-12-31 14:43:53 +0300

    Delete Noel directory

--- Noel/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Noel.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
